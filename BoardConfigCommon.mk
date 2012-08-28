c9c4e40fb4a117623510ef63f2c7225c1337c9d7
Daveee10 <daver16@msn.com>
2012-08-28 19:46:27 +0200

    Add config files, prelink and bluedroid

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,148 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# throughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+TARGET_SPECIFIC_HEADER_PATH := device/semc/msm7x27-common/include
+
+# Camera
+USE_CAMERA_STUB := true
+BOARD_USES_ECLAIR_LIBCAMERA := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+BUILD_CAMERAHAL := true
+TARGET_DISABLE_ARM_PIE := true
+
+# ARMv6-compatible processor rev 5 (v6l)
+TARGET_BOARD_PLATFORM := msm7x27
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+
+BOARD_USE_SCREENCAP := true
+TARGET_OVERLAY_ALWAYS_DETERMINES_FORMAT := true
+TARGET_USE_SOFTWARE_AUDIO_AAC := true
+COPYBIT_MSM7K := true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+
+# GPS
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1240
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := delta
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+TARGET_CUSTOM_BLUEDROID := $(TOP)/device/semc/msm7x27-common/bluedroid.c
+
+# Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# Wifi
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WLAN_DEVICE := wl1271
+BOARD_SOFTAP_DEVICE := wl1271
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+WIFI_DRIVER_MODULE_PATH := /system/lib/modules/tiwlan_drv.ko
+WIFI_DRIVER_MODULE_NAME := tiwlan_drv
+WIFI_FIRMWARE_LOADER := wlan_loader
+WIFI_EXT_MODULE_PATH := /system/lib/modules/sdio.ko
+WIFI_EXT_MODULE_NAME := sdio
+WIFI_AP_DRIVER_MODULE_PATH := /system/lib/modules/tiap_drv.ko
+WIFI_AP_DRIVER_MODULE_NAME := tiap_drv
+WIFI_AP_FIRMWARE_LOADER := wlan_ap_loader
+PRODUCT_DEFAULT_WIFI_CHANNELS := 14
+
+# USB
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
+# CFLAGS
+COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DECLAIR_LIBCAMERA
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+
+# JIT
+WITH_JIT := true
+ENABLE_JSC_JIT:= true
+ARCH_ARM_HAVE_VFP := true
+
+# Browser
+JS_ENGINE := v8
+HTTP := chrome
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# HW
+BOARD_EGL_CFG := device/semc/msm7x27-common/prebuilt/egl.cfg
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_LEGACY_QCOM := false
+TARGET_USES_GENLOCK := true
+TARGET_USES_OVERLAY := false
+TARGET_USES_C2D_COMPOSITION := false
+USE_OPENGL_RENDERER := true
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_USE_SKIA_LCDTEXT := true
+TARGET_NO_HW_VSYNC := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=null
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_PAGE_SIZE := 0x00000800
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p1
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+BOARD_CUSTOM_BOOTIMG_MK := device/semc/msm7x27-common/custombootimg.mk
+
+# Audio
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_COMBO_DEVICE_SUPPORTED := true
+BOARD_USES_AUDIO_LEGACY := false
+
+# Odex
+WITH_DEXPREOPT := false
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# Recovery
+TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := $(TOP)/device/semc/msm7x27-common/recovery/recovery_keys.c
+
+# A custom ota package maker for a device without a boot partition
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/semc/msm7x27-common/releasetools/semc_ota_from_target_files
+
+# Prelinks
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/semc/msm7x27-common/prelink-linux-arm-msm7x27.map
---
+++ msm7x27.mk
@@ -0,0 +1,158 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Sensors, etc...
+PRODUCT_PACKAGES += \
+    gps.delta \
+    sensors.msm7x27 \
+    lights.msm7x27 \
+    audio.a2dp.default \
+    libaudioutils
+
+# GPU
+PRODUCT_PACKAGES += \
+    gralloc.default \
+    gralloc.msm7x27 \
+    hwcomposer.default \
+    hwcomposer.msm7x27 \
+    copybit.msm7x27 \
+    libgenlock \
+    libmemalloc \
+    libtilerenderer \
+    libqdutils \
+    liboverlay
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm7x27
+
+# Omx
+PRODUCT_PACKAGES += \
+    libmm-omxcore \
+    libdivxdrmdecrypt \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw \
+    libopencorehw \
+    libQcomUI
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    screencap \
+    hostap \
+    rzscontrol \
+    rild \
+    com.android.future.usb.accessory \
+    make_ext4fs \
+    setup_fs \
+    wlan_loader \
+    wlan_cu \
+    dhcpcd.conf
+
+# Bugmailer
+PRODUCT_PACKAGES += send_bug
+PRODUCT_COPY_FILES += \
+        system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+        system/extras/bugmailer/send_bug:system/bin/send_bug
+
+# Compcache
+PRODUCT_COPY_FILES += \
+        device/semc/msm7x27-common/prebuilt/xbin/rzscontrol:system/xbin/rzscontrol
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distict.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Config files
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/etc/gps.conf:system/etc/gps.conf \
+    device/semc/msm7x27-common/prebuilt/etc/hw_config.sh:system/etc/hw_config.sh \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+
+# Extra prebuilt binaries
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/framework/com.sonyericsson.suquashi.jar:system/framework/com.sonyericsson.suquashi.jar \
+    device/semc/msm7x27-common/prebuilt/app/Radio.apk:system/app/Radio.apk \
+    device/semc/msm7x27-common/prebuilt/app/SystemConnector.apk:system/app/SystemConnector.apk \
+    device/semc/msm7x27-common/prebuilt/framework/SemcSmfmf.jar:system/framework/SemcSmfmf.jar \
+    device/semc/msm7x27-common/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+    device/semc/msm7x27-common/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/semc/msm7x27-common/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/semc/msm7x27-common/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/semc/msm7x27-common/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    device/semc/msm7x27-common/prebuilt/xbin/remount:system/xbin/remount \
+    device/semc/msm7x27-common/prebuilt/bin=hciattach:system/bin/hciattach \
+    device/semc/msm7x27-common/prebuilt/app/AntHalService.apk:system/app/AntHalService.apk
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/semc/msm7x27-common/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/semc/msm7x27-common/prebuilt/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    device/semc/msm7x27-common/prebuilt/usr/keylayout/systemconnector.kl:system/usr/keylayout/systemconnector.kl \
+    device/semc/msm7x27-common/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl
+
+# Wifi and hotspot
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/bin/dhcpcd:system/bin/dhcpcd \
+    device/semc/msm7x27-common/prebuilt/bin/tiap_loader.sh:system/bin/tiap_loader.sh \
+    device/semc/msm7x27-common/prebuilt/etc/init.d/10dnsconf:system/etc/init.d/10dnsconf \
+    device/semc/msm7x27-common/prebuilt/etc/init.d/10hostapconf:system/etc/init.d/10hostapconf \
+    device/semc/msm7x27-common/prebuilt/etc/wifi/softap/hostapd.conf:system/etc/wifi/softap/hostapd.conf \
+    device/semc/msm7x27-common/prebuilt/etc/wifi/dnsmasq.conf:system/etc/wifi/dnsmasq.conf \
+    device/semc/msm7x27-common/prebuilt/etc/wifi/tiwlan_firmware.bin:system/etc/wifi/tiwlan_firmware.bin \
+    device/semc/msm7x27-common/prebuilt/etc/wifi/softap/tiwlan_firmware_ap.bin:system/etc/wifi/softap/tiwlan_firmware_ap.bin
+
+# A2SD and extra init files
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/bin/a2sd:system/bin/a2sd \
+    device/semc/msm7x27-common/prebuilt/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd \
+    device/semc/msm7x27-common/prebuilt/etc/init.d/05mountext:system/etc/init.d/05mountext \
+    device/semc/msm7x27-common/prebuilt/xbinzipalign:system/xbin/zipalign
+
+# Adreno 200 files
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/lib/libgsl.so:system/lib/libgsl.so \
+    device/semc/msm7x27-common/prebuilt/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    device/semc/msm7x27-common/prebuilt/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    device/semc/msm7x27-common/prebuilt/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    device/semc/msm7x27-common/prebuilt/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    device/semc/msm7x27-common/prebuilt/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    device/semc/msm7x27-common/prebuilt/lib/libC2D2.so:system/lib/libC2D2.so \
+    device/semc/msm7x27-common/prebuilt/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    device/semc/msm7x27-common/prebuilt/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    device/semc/msm7x27-common/prebuilt/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/semc/msm7x27-common/prebuilt/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    device/semc/msm7x27-common/prebuilt/etc/adreno_config.txt:system/etc/adreno_config.txt
+
+# Audio blobs from ICS
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x27-common/prebuilt/lib/hw/audio.primary.msm7x27.so:system/lib/hw/audio.primary.msm7x27.so \
+    device/semc/msm7x27-common/prebuilt/lib/hw/audio_policy.msm7x27.so:system/lib/hw/audio_policy.msm7x27.so
