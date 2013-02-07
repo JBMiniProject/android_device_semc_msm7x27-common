34cae9820da20bbce1c87ab2934db81cfb054987
Daveee10 <daver16@msn.com>
2013-02-07 11:16:47 +0100

    hw_config.sh: fixup frequencies for .32 kernel
    Forked from nobodyAtall

--- prebuilt/etc/hw_config.sh
+++ prebuilt/etc/hw_config.sh
@@ -30,8 +30,8 @@ echo "smartassV2" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
 echo 30 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/down_differential
 echo 500000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-echo 122880 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 245760 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 604800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
 # Flash LED configuration
 dev=/sys/devices/platform/msm_pmic_flash_led
